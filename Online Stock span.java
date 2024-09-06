5e28b7ce848001876ab6327ffeacddc771617ee6
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-06 16:21:30 +0530

    Day 42 Of #CrackYourInternship

---
+++ Online Stock span.java
@@ -0,0 +1,22 @@
+//Day 42 of #CrackYourInternship
+class StockSpanner {
+    Stack<int[]> stack; 
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int result = 1;
+        while(!stack.isEmpty() && stack.peek()[0] <= price){
+            result += stack.pop()[1];
+        }
+        stack.push(new int[] {price, result});
+        return result;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
