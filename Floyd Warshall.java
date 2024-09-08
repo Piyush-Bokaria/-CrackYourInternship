20675325426dad9e4883dfdb1843e6c6d4e40e83
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-08 08:56:09 +0530

    Day 44 Of #CrackYourInternship

---
+++ Floyd Warshall.java
@@ -0,0 +1,20 @@
+//Day 44 of #CrackYourInternship
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        // Code here 
+        int n = matrix.length;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][k] != -1 && matrix[k][j] != -1) {
+                        if (matrix[i][j] == -1 || matrix[i][k] + matrix[k][j] < matrix[i][j]) {
+                            matrix[i][j] = matrix[i][k] + matrix[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
