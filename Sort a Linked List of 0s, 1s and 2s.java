d12516a527975e61ae497667e38f86b3b833af56
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-09 22:50:25 +0530

    Day 45 Of #CrackYourInternship

---
+++ Sort a Linked List of 0s, 1s and 2s.java
@@ -0,0 +1,27 @@
+//Day 45 of #CrackYourInternship
+class GfG {
+  
+    static void sortList(Node head) {
+      
+        int[] cnt = { 0, 0, 0 };
+        Node ptr = head;
+
+        while (ptr != null) {
+            cnt[ptr.data] += 1;
+            ptr = ptr.next;
+        }
+
+        int idx = 0;
+        ptr = head;
+
+        while (ptr != null) {
+            if (cnt[idx] == 0)
+                idx += 1;
+            else {
+                ptr.data = idx;
+                cnt[idx] -= 1;
+                ptr = ptr.next;
+            }
+        }
+    }
+}
