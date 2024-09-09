f97b6aad937edd217211fabfb43a17ac9375f868
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-09 23:53:07 +0530

    Day 45 Of #CrackYourInternship

---
+++ Deque Implementation.java
@@ -0,0 +1,28 @@
+//Day 45 of #CrackYourInternship
+  public static void push_back_pb(ArrayDeque<Integer> dq, int x) {
+    // Your code here
+    dq.addLast(x);
+}
+
+// Function to pop element from back of the deque.
+  public static void pop_back_ppb(ArrayDeque<Integer> dq) {
+    // Your code here
+    if(!dq.isEmpty()){
+        dq.removeLast();
+    }
+}
+
+// Function to return element from front of the deque.
+  public static int front_dq(ArrayDeque<Integer> dq) {
+    // Your code here
+    if(!dq.isEmpty()){
+        return dq.getFirst();
+    }
+    return -1;
+}
+
+// Function to push element x to the front of the deque.
+  public static void push_front_pf(ArrayDeque<Integer> dq, int x) {
+    // Your code here
+    dq.addFirst(x);
+}
