e7d7b60c69423b8ff6f220918be319c86efbe963
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-09 21:42:05 +0530

    Day 45 Of #CrackYourInternship

---
+++ Middle of the Linked List.java
@@ -0,0 +1,22 @@
+//Day 45 of #CrackYourInternship
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(head != null && head.next != null){
+            slow = slow.next;
+            head = head.next.next;
+        }
+        return slow;
+    }
+}
