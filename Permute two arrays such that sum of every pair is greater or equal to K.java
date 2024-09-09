45492cd2190b7c9f187c11064f1c23e376c20aee
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-09 20:47:24 +0530

    Day 45 Of #CrackYourInternship

---
+++ Permute two arrays such that sum of every pair is greater or equal to K.java
@@ -0,0 +1,17 @@
+//Day 45 of #CrackYourInternship
+class GFG 
+{
+    static boolean isPossible(Integer a[], int b[],
+                                      int n, int k) 
+    {
+        Arrays.sort(a, Collections.reverseOrder());
+    
+        Arrays.sort(b);
+    
+        for (int i = 0; i < n; i++)
+        if (a[i] + b[i] < k)
+            return false;
+    
+        return true;
+    }
+}
