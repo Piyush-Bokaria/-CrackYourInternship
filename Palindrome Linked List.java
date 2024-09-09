bdc21ecc2fd2ed0564c6544824ac4263e684f480
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-09 23:27:13 +0530

    Day 45 Of #CrackYourInternship

---
+++ Palindrome Linked List.java
@@ -0,0 +1,41 @@
+//Day 45 of #CrackYourInternship
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = reverse(slow);
+        while(slow != null && head != null){
+            if(head.val != slow.val){
+                return false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+    private ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
