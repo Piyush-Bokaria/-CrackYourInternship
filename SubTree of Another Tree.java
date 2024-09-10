41201acfc2ab5f6776137a82d06be96778bba785
Piyush Bokaria <167098638+Piyush-Bokaria@users.noreply.github.com>
2024-09-10 06:18:46 +0530

    Day 45 Of #CrackYourInternship

---
+++ SubTree of Another Tree.java
@@ -0,0 +1,40 @@
+//Day 45 of #CrackYourInternship
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null){
+            return false;
+        }
+        if(sametree(root, subRoot)){
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean sametree(TreeNode root1, TreeNode root2){
+        if(root1 != null && root2 == null || root1 == null && root2 != null){
+            return false;
+        }
+        if(root1 == null && root2 == null){
+            return true;
+        }
+        if(root1.val != root2.val){
+            return false;
+        }
+        return sametree(root1.left, root2.left) && sametree(root1.right, root2.right);
+    }
+}
